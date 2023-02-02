02947e25b0f25a94b0cc018c66c894659aafc473
sufyan9113 <shaikhsufyan000@gmail.com>
2023-02-03 00:05:52 +0530

    second commit

--- Programs/Var.java
+++ Programs/Var.java
@@ -3,13 +3,26 @@
  class Var {
     public static void main(String[]args){
        
-         int a=5;
-        
-         int b=6;
+      int a=10;
+      int b=10;
+      int c=15;
+    //  boolean result = a>b;
 
-       
-         System.out.println(a);
-   System.out.println(b);
+        
+   System.out.println(a==b);
+   System.out.println(a<b);
+   System.out.println(a>b);
+   System.out.println(a<=b);
+   System.out.println(a>=b);
+   System.out.println(a==c);
+   System.out.println(a<c);
+   System.out.println(a>c);
+   System.out.println(a<=c);
+   System.out.println(a>=c);
+   System.out.println(a!=b);
+
+
+   
     
 }
  }
