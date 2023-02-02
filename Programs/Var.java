9caa0eeb705637d021b88763024a86cd342a67ba
sufyan9113 <shaikhsufyan000@gmail.com>
2023-02-02 23:23:45 +0530

    second commit

---
+++ Programs/Var.java
@@ -0,0 +1,16 @@
+
+
+ class Var {
+    public static void main(String[]args){
+       
+         int a=5;
+        
+         int b;
+         b= a++ + ++a + a++ + ++a + --a + a--;
+
+       
+         System.out.println(a);
+   System.out.println(b);
+    
+}
+ }
