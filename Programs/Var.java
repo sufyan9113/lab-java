34e6a9de10a837f1544e6d89ce5bd2a55fb05ac2
sufyan9113 <shaikhsufyan000@gmail.com>
2023-02-03 19:53:14 +0530

    logicl operator

--- Programs/Var.java
+++ Programs/Var.java
@@ -2,26 +2,38 @@
 
  class Var {
     public static void main(String[]args){
-       
       int a=10;
-      int b=10;
-      int c=15;
-    //  boolean result = a>b;
+      int b=20;
+      int c=30;
+      //boolean d=a>b;
+      System.out.println(a>b && a<b && a>c && a<c);
+      System.out.println(a<b && c>b && a<c && b<c);
+      System.out.println(a<b || c>b || a<c || b<c);
+      System.out.println(a>b || c<b || a>c || b>c);
+      System.out.println(a!=b);
+      System.out.println(a!=c);
+
+
+
+
+
+     /*  System.out.println(a>b);
+      System.out.println(a<b);
+      System.out.println(a==b);
+      System.out.println(a==c);
+      System.out.println(a>c);
+      System.out.println(a<c);
+      System.out.println(a>=b);
+      System.out.println(a<=b);
+      System.out.println(a>=c);
+      System.out.println(a<=c);
+      */
+
+       
+   
 
         
-   System.out.println(a==b);
-   System.out.println(a<b);
-   System.out.println(a>b);
-   System.out.println(a<=b);
-   System.out.println(a>=b);
-   System.out.println(a==c);
-   System.out.println(a<c);
-   System.out.println(a>c);
-   System.out.println(a<=c);
-   System.out.println(a>=c);
-   System.out.println(a!=b);
-
-
+   
    
     
 }
