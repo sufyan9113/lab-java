c18cb19f9f2e937fee62751a14733309e750040e
sufyan9113 <sufyan9113>
2023-02-03 23:53:36 +0530

    if else leddder (nested)

--- Programs/ConditionalOperator.java
+++ Programs/ConditionalOperator.java
@@ -2,12 +2,33 @@
 
     public static void main(String[]args){
      
-        int age=15;
-        if(age>=18){
-            System.out.println("Adult");
+        int age=18;
+        if(age>=18 && age <60){
+            if(age >=18 && age <20){
+                System.out.println("you are in early 20's");
+            }
+            else if(age >=20 && age <30){
+            System.out.println("you are in 20's");
         }
+            else if(age >=30 && age <40){
+            System.out.println("you are in 30's");
+            }
+            else if(age >=40 && age <50){
+                System.out.println("you are in 40's");
+            }
+            else {
+            System.out.println("you are in 50's");
+            }
+        }
+        else if(age >=60){
+            System.out.println("you are old");
+        }
+        else if(age >=16 && age <18){
+            System.out.println("you are teen");
+        }
+        
         else{
-            System.out.println("Minor");
+            System.out.println("you are kid");
         }
     }
     
