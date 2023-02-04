322ea0ff953148182a5e5f6e222649c1409eb011
sufyan9113 <sufyan9113>
2023-02-04 12:47:14 +0530

    turnary nested

--- Programs/Turnary.java
+++ Programs/Turnary.java
@@ -2,37 +2,34 @@ public class Turnary {
 
     public static void main(String[]args){
 
-        int a=10;
-        int b=5;
-        int c=2;
-       
-       int d= a<b?a:b;
-        System.out.println(d);
-        String result= a<b?"a is less": "b is less";
-        System.out.println(result);
+        int a=30;
+        int b=10;
+        int c=20;
 
-        int num= a<b? (a<c? a:c) : b<c? b:c;
-        System.out.println(num);
-
-        if(a<b){
-            if(a<c){
-                System.out.println(a);
-            }
-            else{
-                System.out.println(c);
-            }
-
+        int r1=a>b?a:b;
+        System.out.println(r1);
+        String r2=a>b?"a is Greater":"b is Greater";
+        System.out.println(r2);
+        
+        int r3=a>b? a>c?a:c : b>c? b:c;
+        System.out.println(r3);
+    
+    if(a>b){
+        if(a>c){
+            System.out.println(a);
         }
         else{
-            if(b<c){
-                System.out.println(b);
-
-            }
-            else{
-                System.out.println(c);
-            }
-
+            System.out.println(c);
         }
-    
     }
+    else{
+        if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+
+    }
+}
 }
