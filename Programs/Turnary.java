977526cb2b16ecbd112b08fba33b9cf52d4056c4
sufyan9113 <sufyan9113>
2023-02-04 12:46:57 +0530

    turnary ladder

--- Programs/Turnary.java
+++ Programs/Turnary.java
@@ -3,11 +3,36 @@ public class Turnary {
     public static void main(String[]args){
 
         int a=10;
-        int b=15;
+        int b=5;
+        int c=2;
        
-       int c= a<b?a:b;
-        System.out.println(c);
+       int d= a<b?a:b;
+        System.out.println(d);
+        String result= a<b?"a is less": "b is less";
+        System.out.println(result);
 
+        int num= a<b? (a<c? a:c) : b<c? b:c;
+        System.out.println(num);
+
+        if(a<b){
+            if(a<c){
+                System.out.println(a);
+            }
+            else{
+                System.out.println(c);
+            }
+
+        }
+        else{
+            if(b<c){
+                System.out.println(b);
+
+            }
+            else{
+                System.out.println(c);
+            }
+
+        }
     
     }
 }
