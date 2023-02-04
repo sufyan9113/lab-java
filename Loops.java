619824c2dd69e0c32f255a1e60912be460bd45d8
sufyan9113 <sufyan9113>
2023-02-04 20:04:55 +0530

    exercise

---
+++ Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[]args){
+        int a=0;
+        int b=0;
+
+        for(int i=0; i<5; i++){
+            System.out.println("*");
+        }
+        while(a<5){
+            System.out.println("$");
+            a++;
+
+        }
+        do{
+            System.out.println("@");
+            b++; 
+        }
+        while(b<5);
+    }
+    
+}
---
+++ Programs/Exercise.java
@@ -0,0 +1,73 @@
+ import java.util.Scanner;
+ class Exercise {
+    public static void main(String[]args){
+        
+      /*   char c;
+        System.out.println("enter lower case character");
+        Scanner in= new Scanner(System.in);
+        c= in.next().charAt(0);
+
+        if(c>='a' && c<='z'){
+            if(c=='a'){
+        System.out.println("entered character is 'a' ");
+        }
+        else{
+            System.out.println("enter character is not 'a' ");
+        }
+    }
+    else{
+        System.out.println("enter lower case character");
+    }
+
+/* 
+        int num;
+        System.out.println("enter num between 1 to 10^6");
+        Scanner in= new Scanner(System.in);
+        num= in.nextInt();
+
+        if(num>1 && num<=1000000){
+            if(num%2==0){
+                System.out.println("Even");
+            }
+            else{
+                System.out.println("Odd");
+            }
+        }
+            else{
+                System.out.println("enter num between 1 to 10^6 ");
+            }
+        
+
+        /* 
+
+            for(int i=1; i<=100; i++){
+                if(!(i%5==0 || i%7==0)){
+                System.out.println(i);
+                }
+            }        
+       /*  int num;
+        System.out.println("enter number");
+        Scanner in= new Scanner(System.in);
+        num= in.nextInt();
+
+        if(num%3==0 && num%5==0){
+            System.out.println("FizzBuzz");
+        }
+        else if(num % 3==0){
+            System.out.println("Fizz");
+        }
+        else if(num % 5==0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(num);
+        }
+        */
+
+    
+
+
+
+    }
+    
+}
