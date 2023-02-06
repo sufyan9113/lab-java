98f9f248cf6e1a1fb41395f6f4c20308e87b08ca
sufyan9113 <sufyan9113>
2023-02-07 00:04:30 +0530

    complex

---
+++ Programs/Loops/Complex.java
@@ -0,0 +1,19 @@
+package Loops;
+
+public class Complex {
+    public static void main(String[]args){
+        int n=10;
+        for (int i=0; i<=n; i++){
+            for(int j=0; j<=n; j++){
+            if(i==j){
+                System.out.print("*");
+        }
+        else{
+            System.out.print(" ");
+        }
+}
+    System.out.println();
+    
+}
+}
+}
