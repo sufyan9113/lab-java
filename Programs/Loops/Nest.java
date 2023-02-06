cade4fc54f6468a41c7be0a05be28b77914ed53a
sufyan9113 <sufyan9113>
2023-02-06 22:44:04 +0530

    pattern

--- Programs/Loops/Nest.java
+++ Programs/Loops/Nest.java
@@ -4,12 +4,114 @@ public class Nest {
 
     public static void main(String[]args){
 
-        for(int i=1; i<=10; i++){
-            for (int j=1; j<=10; j++){
-                System.out.print(j + " ");
+        int n=10;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j<(n-1)/2 || i==n-1 && j<(n-1)/2 || j==0 || j==(n-1)/2 && i>0 && i<n-1){
+                System.out.print("*");
             }
-            System.out.println(i +" ");
+            else{
+                System.out.print(" ");
+            }
         }
+            System.out.println();
+        }
+
+        /*int n=9;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j>0 && j<(n-1)/2 ||j==0 && i>0|| j==(n-1)/2 && i>0 || i==(n-1)/2  && j<=(n-1)/2){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+
+       /* int n=5;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0||j==0 || i==(n-1)/2){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+
+       /* int n=5;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0|| i==n-1||j==0 || i==(n-1)/2){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+        /*
+        int n=5;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==0|| i==n-1|| j==(n-1)/2){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+
+      /*  int n=6;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if(i==(n-1)/2|| j==0|| j==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+
+        /*int n=10;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if(i==0|| i==(n-1)/2|| j==0|| j==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+
+        /*
+        int n=5;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if(i==0|| i==n-1|| j==0|| j==n-1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            System.out.println();
+        }
+        */
     }
     
 }
