154b224dbf3ebbf8485e61dfbba865336be880b7
sufyan9113 <sufyan9113>
2023-02-10 21:56:55 +0530

    day  9 assignment

--- Programs/Loops/Assignment9.java
+++ Programs/Loops/Assignment9.java
@@ -4,7 +4,7 @@ public class Assignment9 {
    
     public static void main(String[]args){
         //Answer 4
-        int n=10;
+        int n=11;
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
         if(i==0 && j>0 || j==0 && i<=(n-1)/2 && i>0 && i<(n-1)/2 || i==(n-1)/2 && j>0 && j<n-1 || j==n-1 && i>(n-1)/2 && i<n-1 || i==n-1 && j<n-1 ){
@@ -14,6 +14,7 @@ public class Assignment9 {
                 System.out.print(" ");
                 }
             }
+    
             System.out.print("  ");
             for(int j=0; j<n; j++){
                 if(j==0 && i<n-1|| j==n-1 && i<n-1 || i==n-1 && j>0 && j<n-1){
@@ -41,11 +42,86 @@ public class Assignment9 {
                     System.out.print(" ");
                      }
              }
-
+             System.out.print("  ");
+            for(int j=0; j<n; j++){
+            if(i==0 && j>0 && j<n-1|| j==0 && i>0 || j==n-1 && i>0||  i==(n-1)/2){
+                System.out.print("*");
+                 }
+                else{
+                    System.out.print(" ");
+                     }
+             }
+             System.out.print("  ");
+            for(int j=0; j<n; j++){
+            if( j==0  || j==n-1 || i==j){
+                System.out.print("*");
+                 }
+                else{
+                    System.out.print(" ");
+                     }
+             }
+             System.out.println();
+            }
             System.out.println();
-        }
 
-        /*
+            for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+            if( i==0 && j>0 || i==n-1 && j<n-1 || i==(n-1)/2 && j>0 && j<n-1 || j==0 && i>0 && i<(n-1)/2 || j==n-1 && i>(n-1)/2 && i<n-1 ){
+                System.out.print("*");
+                 }
+                else{
+                    System.out.print(" ");
+                     }
+             } 
+             System.out.print("  ");
+            for(int j=0; j<n; j++){
+            if( j==0 || j==n-1 || i==(n-1)/2 ){
+                System.out.print("*");
+                 }
+                else{
+                    System.out.print(" ");
+                     }
+             }
+             System.out.print("  ");
+            for(int j=0; j<n; j++){
+            if( i==0  && j>0 && j<n-1|| i==(n-1)/2 || j==0 && i>0 || j==n-1 && i>0){
+                System.out.print("*");
+                 }
+                else{                   
+                    System.out.print(" ");
+                     }
+             }
+             System.out.print("  ");
+             for(int j=0; j<n; j++){
+                if( i==0 || i==n-1 || j==(n-1)/2){
+                    System.out.print("*");
+                     }
+                    else{                   
+                        System.out.print(" ");
+                         }
+                 }
+                 System.out.print("  ");
+                 for(int j=0; j<n; j++){
+                    if( j==0 || i+j==(n-1)/2 || i-j==(n-1)/2){
+                        System.out.print("*");
+                         }
+                        else{                   
+                            System.out.print(" ");
+                             }
+                }
+                System.out.print("  ");
+                 for(int j=0; j<n; j++){
+                    if( j==0 || j==n-1 || i==(n-1)/2){
+                        System.out.print("*");
+                         }
+                        else{                   
+                            System.out.print(" ");
+                             }
+                }           
+             System.out.println();
+        }
+    
+    /* 
         int n=11;
 
         for(int i=0; i<n; i++){
@@ -269,6 +345,7 @@ public class Assignment9 {
             System.out.println();
         }
         */
-    }
+    
+}
     
 }
