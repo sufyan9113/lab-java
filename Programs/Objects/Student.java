dab20db914ac150e51c9fd1566f29352c9f336d5
sufyan9113 <sufyan9113>
2023-02-11 21:42:36 +0530

    object

---
+++ Programs/Objects/Student.java
@@ -0,0 +1,17 @@
+package Objects;
+
+public class Student {
+
+    int num=9113;
+    String name="Sufyan";
+
+    public static void main(String[]args){
+
+        Student s1= new Student();
+        
+        System.out.println("Name:"+s1.name+" Number: "+s1.num);
+
+    }
+
+    
+}
